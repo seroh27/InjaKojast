e2c7386ec90330339b8cb4fb8dff637bc8ecf042
Sero Hartounian <sero_h27@yahoo.com>
2024-07-02 16:07:43 +0330

    map added

---
+++ app/src/main/java/com/example/whereisthis/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.whereisthis;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private RelativeLayout layout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        webView = findViewById(R.id.webView);
+        layout = findViewById(R.id.rootLayout); // This references the root RelativeLayout
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/map.html");
+
+        addCityButtons();
+    }
+    private void addCityButtons() {
+        int[][] cityCoordinates = {
+                {360, 600}, //tehran
+                {410, 1100},//fars
+                {550, 850},//yazd
+                {380, 800},//isfahan
+                {500, 610},//semnan
+                {700, 610},//kh razavi
+                {740, 900},//south khorasan
+                {650, 1050},//Kerman
+                {810, 1200},//sistan baluchestan
+                {600, 1250},//Hormozgan
+                {510, 480},//Golestan
+                {620, 450},//north khorasan
+                {380, 530},//Mazandaran
+                {320, 1130},//Mazandaran
+                {330, 670},//Qom
+                {5, 440},//West Azerbaijan
+                {90, 420},//East Azerbaijan
+                {170, 360},//Ardabil
+                {200, 930},//Khuzistan
+                {180, 780},//Lorestan
+                {100, 600},//Kurdistan
+                {80, 700},//Kermanshah
+                {90, 800},//Ilam
+                {290, 900},//"Charmahal Bakhtiari"
+                {300, 970},//"Kohgiluyeh and Boyer-Ahmad"
+                {180, 520},//Zanjan
+                {230, 450},//Gilan
+                {260, 550},//Qazvin
+                {260,700},//Markazi
+                {180,670},//Hamedan
+        };
+
+        String[] cityNames = {"Tehran", "Fars","Yazd","Isfahan"
+                ,"Semnan","Khorasan Razavi",
+                "South Khorasan","Kerman",
+                "Sistan Baluchestan","Hormozgan"
+                ,"Golestan","North Khorasan","Mazandaran","Bushehr",
+                "Qom","West Azerbaijan","East Azerbaijan","Ardabil",
+                "Khuzistan","Lorestan","Kurdistan","Kermanshah","Ilam","Charmahal Bakhtiari",
+        "Kohgiluyeh and Boyer-Ahmad","Zanjan","Gilan","Qazvin","Markazi","Hamedan"};
+
+        for (int i = 0; i < cityCoordinates.length; i++) {
+            int[] coord = cityCoordinates[i];
+            String cityName = cityNames[i];
+
+            Button cityButton = new Button(this);
+            RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                    90,
+                    90);
+            params.leftMargin = coord[0];
+            params.topMargin = coord[1];
+            cityButton.setLayoutParams(params);
+            cityButton.setBackgroundResource(R.drawable.circular_button);
+            cityButton.setText(cityName);
+//            cityButton.setBackgroundColor(android.graphics.Color.RED);
+            cityButton.setTextColor(android.graphics.Color.WHITE);
+            cityButton.setTextSize(7);
+
+            cityButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onCityClick(cityName);
+                }
+            });
+
+            layout.addView(cityButton);
+        }
+    }
+
+    private void onCityClick(String city) {
+        // Handle city click
+        Toast.makeText(this, "City clicked: " + city, Toast.LENGTH_SHORT).show();
+        // You can navigate to another activity or display more information here
+    }
+
+}
